db6ac3566cb9944aebca6d5aa330ea7f4164b96b
victoria <sunnystella24@gmail.com>
2014-03-28 10:17:46 -0700

    권한설정 처리

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -4,24 +4,25 @@
 var regx_id =/^[a-zA-Z0-9]+\@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}(?:\.[a-zA-Z]{2})?$/;   
 var regx_pwd = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{6,12}$/;
 var regx_name = /^[가-힣a-zA-Z]+$/;
+var regx = /^[a-zA-Z]{4}+\_[a-zA-Z]$/;
 
 function loginChk(){
 	
-	var id = document.getElementsByName("j_username")[0].value;
-	var pwd = document.getElementsByName("j_password")[0].value; 
+	var id = document.getElementsByName("userid")[0].value;
+	var pwd = document.getElementsByName("userpwd")[0].value; 
 
 	/* 
 	 * 아이디는 이메일 형식만 입력 
 	 */
 	if((id.length) == 0 || (id == null)){
 		alert("아이디를 입력하세요.");
-		document.getElementsByName("j_username")[0].value="";
-		document.getElementsByName("j_username")[0].focus();
+		document.getElementsByName("userid")[0].value="";
+		document.getElementsByName("userid")[0].focus();
 		return false;
 	}else if(!regx_id.test(id)){
 		alert("아이디는 이메일 형식으로 입력하세요.");
-		document.getElementsByName("j_username")[0].value="";
-		document.getElementsByName("j_username")[0].focus();
+		document.getElementsByName("userid")[0].value="";
+		document.getElementsByName("userid")[0].focus();
 		return false;
 	}
 	
@@ -31,18 +32,18 @@ function loginChk(){
 	 */
 	if((pwd.length) == 0 || (pwd == null)){
 		alert("비밀번호를 입력하세요.");
-		document.getElementsByName("j_password")[0].value="";
-		document.getElementsByName("j_password")[0].focus();
+		document.getElementsByName("userpwd")[0].value="";
+		document.getElementsByName("userpwd")[0].focus();
 		return false;
 	}else if((pwd.length) < 6 || (pwd.length) > 12){
 		alert("비밀번호는 6 ~ 12자리로 입력하세요.");
-		document.getElementsByName("j_password")[0].value="";
-		document.getElementsByName("j_password")[0].focus();
+		document.getElementsByName("userpwd")[0].value="";
+		document.getElementsByName("userpwd")[0].focus();
 		return false;
 	}else if(!regx_pwd.test(pwd)){
 		alert("비밀번호는 문자,숫자,특수문자의 조합으로 입력하세요.");
-		document.getElementsByName("j_password")[0].value="";
-		document.getElementsByName("j_password")[0].focus();
+		document.getElementsByName("userpwd")[0].value="";
+		document.getElementsByName("userpwd")[0].focus();
 		return false;
 	}
 	document.getElementById("loginform").submit();
@@ -134,11 +135,6 @@ function updateChk(){
 		document.getElementsByName("username")[0].value="";
 		document.getElementsByName("username")[0].focus();
 		return false;
-	}else if(!regx_name.test(name)){
-		alert("이름은 한글,영문으로 입력하세요.");
-		document.getElementsByName("username")[0].value="";
-		document.getElementsByName("username")[0].focus();
-		return false;
 	}
 	
 	/* 
@@ -188,19 +184,11 @@ function updateChk(){
 	}
 	
 	/* 
-	 * 권한은 한글/영문만 입력
+	 * 권한은 영문만 입력
 	 */
 	if((role.length) == 0 || (role == null)){
 		alert("권한을 부여하세요.");
-		document.getElementsByName("role")[0].focus();
-		return false;
-	}else if(!regx_name.test(role)){
-		alert("이름은 한글,영문으로 입력하세요.");
-		document.getElementsByName("role")[0].value="";
-		document.getElementsByName("role")[0].focus();
-		return false;
-	}else if(role == 'Admin' || role == 'User'){
-		alert("권한을 재확인하세요.");
+		document.getElementsByName("userrole")[0].focus();
 		return false;
 	}
 	document.getElementById("updateform").submit();
