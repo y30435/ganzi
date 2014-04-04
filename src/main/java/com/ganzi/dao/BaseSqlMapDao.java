3a0b96a517eba66b67d51ee23f79110ef33b6aa8
jhr <jhr@wemakeprice.com>
2014-04-04 14:26:28 +0900

    ibatis -> mybatis

--- src/main/java/com/ganzi/dao/BaseSqlMapDao.java
+++ src/main/java/com/ganzi/dao/BaseSqlMapDao.java
@@ -13,74 +13,74 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class BaseSqlMapDao {
-    @Resource(name = "SqlMapClientTemplate")
-    private SqlMapClientTemplate SqlMapClientTemplate;
-    
-    /**
-     * execute insert.
-     * @param queryId
-     * @param parameterObject
-     * @return Object
-     */
-    protected boolean insert(String queryId, Object parameterObject) {
-    	return SqlMapClientTemplate.update(queryId, parameterObject) > 0 ? true : false ;
-    }
-
-    /**
-     * execute update.
-     * @param queryId
-     * @param parameterObject
-     * @return int
-     */
-    protected boolean update(String queryId, Object parameterObject) {
-    	return SqlMapClientTemplate.update(queryId, parameterObject) > 0 ? true : false ;
-    }
-
-    /**
-     * execute delete.
-     * @param queryId
-     * @param parameterObject
-     * @return int
-     */
-    protected boolean delete(String queryId, Object parameterObject) {
-    	return SqlMapClientTemplate.delete(queryId, parameterObject) > 0 ? true : false ;
-    }
-
-    /**
-     * execute queryForList.
-     * @param queryId
-     * @param parameterObject
-     * @return List<?>
-     */
-    protected List<?> list(String queryId, Object parameterObject) {
-    	return SqlMapClientTemplate.queryForList(queryId, parameterObject);
-    }
-    
-    /**
-     * execute queryForList.
-     * @param queryId
-     * @param parameterObject
-     * @return List<?>
-     */
-    protected List<?> list(String queryId) {
-    	return SqlMapClientTemplate.queryForList(queryId);
-    }
-
-    /**
-     * execute queryForObject
-     * @param queryId
-     * @return Object
-     */
-    protected Object valueObject(String queryId, Object parameterObject) {
-    	return SqlMapClientTemplate.queryForObject(queryId, parameterObject);
-    }
-    
-    /**
-     * execute queryForObject
-     * @param queryId
-     * @return Object
-     */
-    protected Object valueObject(String queryId) {
-    	return SqlMapClientTemplate.queryForObject(queryId);
-    }
+//    @Resource(name = "SqlMapClientTemplate")
+//    private SqlMapClientTemplate SqlMapClientTemplate;
+//    
+//    /**
+//     * execute insert.
+//     * @param queryId
+//     * @param parameterObject
+//     * @return Object
+//     */
+//    protected boolean insert(String queryId, Object parameterObject) {
+//    	return SqlMapClientTemplate.update(queryId, parameterObject) > 0 ? true : false ;
+//    }
+//
+//    /**
+//     * execute update.
+//     * @param queryId
+//     * @param parameterObject
+//     * @return int
+//     */
+//    protected boolean update(String queryId, Object parameterObject) {
+//    	return SqlMapClientTemplate.update(queryId, parameterObject) > 0 ? true : false ;
+//    }
+//
+//    /**
+//     * execute delete.
+//     * @param queryId
+//     * @param parameterObject
+//     * @return int
+//     */
+//    protected boolean delete(String queryId, Object parameterObject) {
+//    	return SqlMapClientTemplate.delete(queryId, parameterObject) > 0 ? true : false ;
+//    }
+//
+//    /**
+//     * execute queryForList.
+//     * @param queryId
+//     * @param parameterObject
+//     * @return List<?>
+//     */
+//    protected List<?> list(String queryId, Object parameterObject) {
+//    	return SqlMapClientTemplate.queryForList(queryId, parameterObject);
+//    }
+//    
+//    /**
+//     * execute queryForList.
+//     * @param queryId
+//     * @param parameterObject
+//     * @return List<?>
+//     */
+//    protected List<?> list(String queryId) {
+//    	return SqlMapClientTemplate.queryForList(queryId);
+//    }
+//
+//    /**
+//     * execute queryForObject
+//     * @param queryId
+//     * @return Object
+//     */
+//    protected Object valueObject(String queryId, Object parameterObject) {
+//    	return SqlMapClientTemplate.queryForObject(queryId, parameterObject);
+//    }
+//    
+//    /**
+//     * execute queryForObject
+//     * @param queryId
+//     * @return Object
+//     */
+//    protected Object valueObject(String queryId) {
+//    	return SqlMapClientTemplate.queryForObject(queryId);
+//    }
 }
